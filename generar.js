0a04ce8f6dd2f5baebcc3964d76f9fed49294140
SirYorch <100741077+SirYorch@users.noreply.github.com>
2022-04-13 21:16:56 -0500

    Delete generar.js

--- generar.js
+++
@@ -1,8 +0,0 @@
-// funcion añadir cartas
-
-let conjuntoUno = [`🐱‍🚀`,`🐱‍🏍`,`😡`,`💀`,`🎈`]
-let conjuntoDos = conjuntoUno.concat([`🥼`,`🍔`,`🥩`,`🍤`])
-let conjuntoTres = conjuntoDos.concat([`🥟`,`👩‍🦱`,`👩‍🎓`,`👨‍✈️`])
-let conjuntoCuatro = conjuntoTres.concat([`🤍`,`💖`,`🖤`,`🌍`])
-let conjunto;
-let mesa = document.querySelector(`.mesa`);
